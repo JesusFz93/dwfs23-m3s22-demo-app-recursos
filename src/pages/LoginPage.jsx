de00db5cbfe21b8647e640f7e8f46d476345d619
jfernandez <jfernandez@megatechway.com>
2024-02-20 12:25:00 -0600

    inicio

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,16 @@
+const LoginPage = () => {
+  return (
+    <>
+      <header className="row col">
+        <h1>Login Page</h1>
+      </header>
+      <main className="row">
+        <article className="col">
+          <p>Formulario de Inicio de sesion</p>
+        </article>
+      </main>
+    </>
+  );
+};
+
+export default LoginPage;
